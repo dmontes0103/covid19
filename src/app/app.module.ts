34ec99d3876b53463689a0551eca970ef05fc1ba
dmontes0103 <dmontes0103@gmail.com>
2020-03-26 22:39:46 -0600

    More data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    MainDashboardComponent
+    MainDashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/data/data.js
+++ src/app/data/data.js
@@ -1,104 +1,165 @@
-export default[
-    {
-        "name":"Grecia",
-        "latlng":[10.0726507
-            ,-84.3117298],
-        "cases":9
-    },
-    {
-        "name":"Alajuela",
-        "latlng":[10.0169109
-            ,-84.2156165],
-        "cases":45
-    },
-    {
-        "name":"San Jose",
-        "latlng":[9.9333669
-            ,-84.0858859],
-        "cases":23
-    },
-    {
-        "name":"Santa Ana",
-        "latlng":[9.9327098,-84.1823177],
-        "cases":16
-    }
-    ,
-    {
-        "name":"Escazu",
-        "latlng":[9.9194401,-84.1413027],
-        "cases":13
-    },
-    {
-        "name":"Tibas",
-        "latlng":[9.9601982,-84.0822821],
-        "cases":12
-    },
-    {
-        "name":"Heredia",
-        "latlng":[9.9975216,-84.1211849],
-        "cases":12
-    }    ,
-    {
-        "name":"Desamparados",
-        "latlng":[9.8965978,-84.0631713],
-        "cases":12
-    }
-    ,
-    {
-        "name":"Goicoechea",
-        "latlng":[9.9473717,-84.0179533],
-        "cases":8
-    },
-    {
-        "name":"La Union",
-        "latlng":[9.9388468,-84.0078108],
-        "cases":6
-    },
-    {
-        "name":"Moravia",
-        "latlng":[9.9618075,-84.0532257],
-        "cases": 6
-    },
-    {
-        "name":"San Carlos",
-        "latlng":[10.3378529,-84.4408188],
-        "cases":6
-    },
-    {
-        "name":"Cartago",
-        "latlng":[9.8633303,-83.9245327],
-        "cases":5
-    },
-    {
-        "name":"Curridabat",
-        "latlng":[9.9134323,-84.0332696],
-        "cases":5
-    },
-    {
-        "name":"Montes de Oca",
-        "latlng":[9.9381134,-84.045481],
-        "cases":5
-    }
-    ,
-    {
-        "name":"Poas",
-        "latlng":[10.0744457,-84.2479653],
-        "cases":5
-    },
-    {
-        "name":"Nicoya",
-        "latlng":[10.1494207,-85.4711438],
-        "cases":3
-    },
-    {
-        "name":"Perez Zeledon",
-        "latlng":[9.3535614,-83.7278156],
-        "cases":3
-    },
-    {
-        "name":"Puntarenas",
-        "latlng":[9.9785489,-84.8573261],
-        "cases":3
-    }
-
-]
+export default [{
+  "name": "Grecia",
+  "latlng": [10.0726507, -84.3117298],
+  "cases": 9
+}, {
+  "name": "Alajuela",
+  "latlng": [10.0169109, -84.2156165],
+  "cases": 45
+}, {
+  "name": "San Jose",
+  "latlng": [9.9333669, -84.0858859],
+  "cases": 23
+}, {
+  "name": "Santa Ana",
+  "latlng": [9.9327098, -84.1823177],
+  "cases": 16
+}, {
+  "name": "Escazu",
+  "latlng": [9.9194401, -84.1413027],
+  "cases": 13
+}, {
+  "name": "Tibas",
+  "latlng": [9.9601982, -84.0822821],
+  "cases": 12
+}, {
+  "name": "Heredia",
+  "latlng": [9.9975216, -84.1211849],
+  "cases": 12
+}, {
+  "name": "Desamparados",
+  "latlng": [9.8965978, -84.0631713],
+  "cases": 12
+}, {
+  "name": "Goicoechea",
+  "latlng": [9.9473717, -84.0179533],
+  "cases": 8
+}, {
+  "name": "La Union",
+  "latlng": [9.9388468, -84.0078108],
+  "cases": 6
+}, {
+  "name": "Moravia",
+  "latlng": [9.9618075, -84.0532257],
+  "cases": 6
+}, {
+  "name": "San Carlos",
+  "latlng": [10.3378529, -84.4408188],
+  "cases": 6
+}, {
+  "name": "Cartago",
+  "latlng": [9.8633303, -83.9245327],
+  "cases": 5
+}, {
+  "name": "Curridabat",
+  "latlng": [9.9134323, -84.0332696],
+  "cases": 5
+}, {
+  "name": "Montes de Oca",
+  "latlng": [9.9381134, -84.045481],
+  "cases": 5
+}, {
+  "name": "Poas",
+  "latlng": [10.0744457, -84.2479653],
+  "cases": 5
+}, {
+  "name": "Nicoya",
+  "latlng": [10.1494207, -85.4711438],
+  "cases": 3
+}, {
+  "name": "Perez Zeledon",
+  "latlng": [9.3535614, -83.7278156],
+  "cases": 3
+}, {
+  "name": "Puntarenas",
+  "latlng": [9.9785489, -84.8573261],
+  "cases": 3
+}, {
+  "name": "Santa Cruz",
+  "latlng": [10.2602518, -85.5885873],
+  "cases": 2
+}, {
+  "name": "Atenas		",
+  "latlng": [9.97718, -84.379997],
+  "cases": 2
+}, {
+  "name": "Santa Barbara	",
+  "latlng": [10.03819, -84.15888],
+  "cases": 2
+}, {
+  "name": "Vazquez de Coronado",
+  "latlng": [9.9775608,-84.0083074],
+  "cases": 2,
+}, {
+  "name": "San Rafael		",
+  "latlng": [10.0128373, -84.106983, 15],
+  "cases": 2
+}, {
+  "name": "Aserri		",
+  "latlng": [9.865107, -84.1027104],
+  "cases": 2
+}, {
+  "name": "Coto Brus	",
+  "latlng": [8.9540659, -83.0791588],
+  "cases": 2
+}, {
+  "name": "Jimenez",
+  "latlng": [10.1994596, -83.7836826],
+  "cases": 2
+}, {
+  "name": "El Guarco",
+  "cases": 2,
+  "latlng": [9.8431777, -83.9537617]
+}, {
+  "name": "Garabito	",
+  "cases": 2,
+  "latlng": [9.9778492, -84.8316098]
+}, {
+  "name": "Oreamuno",
+  "cases": 2,
+  "latlng": [9.903667, -83.9767081]
+}, {
+  "name": "Sarchi	",
+  "cases": 2,
+  "latlng": [10.0919309, -84.3558599]
+}, {
+  "name": "Barva	",
+  "cases": 1,
+  "latlng": [10.0202225, -84.1279471]
+}, {
+  "name": "Limon		",
+  "cases": 1,
+  "latlng": [9.9914448, -83.0325223]
+}, {
+  "name": "Palmares	",
+  "cases": 1,
+  "latlng": [10.056764, -84.4347075]
+}, {
+  "name": "Liberia		",
+  "cases": 1,
+  "latlng": [10.6317634, -85.4433859]
+}, {
+  "name": "Montes de Oro",
+  "cases": 1,
+  "latlng": [10.0919364, -84.7311813]
+}, {
+  "name": "Belen		",
+  "cases": 1,
+  "latlng": [9.9852018, -84.1852407]
+}, {
+  "name": "Flores		",
+  "cases": 1,
+  "latlng": [10.0070839, -84.1616868]
+}, {
+  "name": "Alvarado	",
+  "cases": 1,
+  "latlng": [9.9162158, -83.8183322]
+}, {
+  "name": "Orotina	",
+  "cases": 1,
+  "latlng": [9.9122007, -84.5284045]
+}, {
+  "name": "Tilaran	",
+  "latlng": [10.4670143, -84.972916],
+  "cases": 1
+}]
